9ba7d2e781c02f06f8623883cb362e21e6ac154b
SaurabhThakre <saurabhthakre165@gmail.com>
2020-03-07 16:39:07 +0530

    Extra

--- Extra/extra.js
+++ Extra/extra.js
@@ -1,94 +1,94 @@
-    document.onkeydown = function(event){
-        // console.log(event);
-        if (event.keyCode === 37){ // PACMAN MOVE LEFT
-            for (var i = 1; i <= 13; i++) {
-                for (var j = 1; j <= 14; j++) {
-                    var myId = "#id" + i + "id" + j;
-                        if($(myId).hasClass("pacman") == true){
-                            // alert("Have Pacman");
-                            if(arr[i][j-1] == 0){
-                                $(myId).toggleClass("pacman");
-                                var myId1 = "#id" + i + "id" + (j - 1);
-                            }
-                        }
-                }
-            }
-            $(myId1).addClass("pacman");
-        }
-        else if (event.keyCode === 38){ // PACMAN MOVE UP
-            for (var i = 1; i <= 13; i++) {
-                for (var j = 1; j <= 14; j++) {
-                    var myId = "#id" + i + "id" + j;
-                        if($(myId).hasClass("pacman") == true){
-                            // alert("Have Pacman");
-                            if(arr[i-1][j] == 0){
-                                $(myId).toggleClass("pacman");
-                                var myId1 = "#id" + (i - 1) + "id" + j;
-                            }
-                        }
-                }
-            }
-            $(myId1).addClass("pacman");
-        }
-        else if (event.keyCode === 39){ // PACMAN MOVE RIGHT
-            for (var i = 1; i <= 13; i++) {
-                for (var j = 1; j <= 14; j++) {
-                    var myId = "#id" + i + "id" + j;
-                        if($(myId).hasClass("pacman") == true){
-                            // alert("Have Pacman");
-                            if(arr[i][j+1] == 0){
-                                $(myId).toggleClass("pacman");
-                                var myId1 = "#id" + i + "id" + (j + 1);
-                            }
-                        }
-                }
-            }
-            $(myId1).addClass("pacman");
-        }
-        else if (event.keyCode === 40){ // PACMAN MOVE DOWN
-            for (var i = 1; i <= 13; i++) {
-                for (var j = 1; j <= 14; j++) {
-                    var myId = "#id" + i + "id" + j;
-                        if($(myId).hasClass("pacman") == true){
-                            // alert("Have Pacman");
-                            if(arr[i+1][j] == 0){
-                                $(myId).toggleClass("pacman");
-                                var myId1 = "#id" + (i + 1) + "id" + j;
-                            }
-                        }
-                }
-            }
-            $(myId1).addClass("pacman");
-        }
-    }
+    // document.onkeydown = function(event){
+    //     // console.log(event);
+    //     if (event.keyCode === 37){ // PACMAN MOVE LEFT
+    //         for (var i = 1; i <= 13; i++) {
+    //             for (var j = 1; j <= 14; j++) {
+    //                 var myId = "#id" + i + "id" + j;
+    //                     if($(myId).hasClass("pacman") == true){
+    //                         // alert("Have Pacman");
+    //                         if(arr[i][j-1] == 0){
+    //                             $(myId).toggleClass("pacman");
+    //                             var myId1 = "#id" + i + "id" + (j - 1);
+    //                         }
+    //                     }
+    //             }
+    //         }
+    //         $(myId1).addClass("pacman");
+    //     }
+    //     else if (event.keyCode === 38){ // PACMAN MOVE UP
+    //         for (var i = 1; i <= 13; i++) {
+    //             for (var j = 1; j <= 14; j++) {
+    //                 var myId = "#id" + i + "id" + j;
+    //                     if($(myId).hasClass("pacman") == true){
+    //                         // alert("Have Pacman");
+    //                         if(arr[i-1][j] == 0){
+    //                             $(myId).toggleClass("pacman");
+    //                             var myId1 = "#id" + (i - 1) + "id" + j;
+    //                         }
+    //                     }
+    //             }
+    //         }
+    //         $(myId1).addClass("pacman");
+    //     }
+    //     else if (event.keyCode === 39){ // PACMAN MOVE RIGHT
+    //         for (var i = 1; i <= 13; i++) {
+    //             for (var j = 1; j <= 14; j++) {
+    //                 var myId = "#id" + i + "id" + j;
+    //                     if($(myId).hasClass("pacman") == true){
+    //                         // alert("Have Pacman");
+    //                         if(arr[i][j+1] == 0){
+    //                             $(myId).toggleClass("pacman");
+    //                             var myId1 = "#id" + i + "id" + (j + 1);
+    //                         }
+    //                     }
+    //             }
+    //         }
+    //         $(myId1).addClass("pacman");
+    //     }
+    //     else if (event.keyCode === 40){ // PACMAN MOVE DOWN
+    //         for (var i = 1; i <= 13; i++) {
+    //             for (var j = 1; j <= 14; j++) {
+    //                 var myId = "#id" + i + "id" + j;
+    //                     if($(myId).hasClass("pacman") == true){
+    //                         // alert("Have Pacman");
+    //                         if(arr[i+1][j] == 0){
+    //                             $(myId).toggleClass("pacman");
+    //                             var myId1 = "#id" + (i + 1) + "id" + j;
+    //                         }
+    //                     }
+    //             }
+    //         }
+    //         $(myId1).addClass("pacman");
+    //     }
+    // }
     
     
-    let ival = 2;
-    let jval = 2;
+    // let ival = 2;
+    // let jval = 2;
 
-    $("input").change(function(){
-        ival = $('#ival').val();
-        jval = $('#jval').val();
-        console.log(ival);
-        console.log(jval);
-        if(arr[ival][jval] == 0){
-            var myId = "#id" + ival + "id" + jval;
-            $(myId).addClass("goalcolor");
-        }
-        else{
-            alert("There is Wall! Select another position.");
-        }
+    // $("input").change(function(){
+    //     ival = $('#ival').val();
+    //     jval = $('#jval').val();
+    //     console.log(ival);
+    //     console.log(jval);
+    //     if(arr[ival][jval] == 0){
+    //         var myId = "#id" + ival + "id" + jval;
+    //         $(myId).addClass("goalcolor");
+    //     }
+    //     else{
+    //         alert("There is Wall! Select another position.");
+    //     }
         
-    });
+    // });
 
-    $('button').click(function(){
+    // $('button').click(function(){
         
-    });
+    // });
 
 
-    $( "div" ).click(function() {
-        var myide = $( this ).attr("id");
-      });
+    // $( "div" ).click(function() {
+    //     var myide = $( this ).attr("id");
+    //   });
 
        // function DFS(root, goal){
     //     let s = new Stack(200);
@@ -550,18 +550,18 @@
 
     // });
 
-    for (var i = 1; i <= 13; i++) {
-        for (var j = 1; j <= 14; j++) {
-            var myId = "id" + i + "id" + j;
-            var myIdr = "id" + i + "id" + (j + 1);
-            var myIdd = "id" + (i + 1) + "id" + j;
-            if(arr[i][j] == 0){
-                if(arr[i][j + 1] == 0){
-                    g.addEdge(myId, myIdr);
-                }
-                if(arr[i + 1][j] == 0){
-                    g.addEdge(myId, myIdd);
-                }
-            }
-        }
-    }
+    // for (var i = 1; i <= 13; i++) {
+    //     for (var j = 1; j <= 14; j++) {
+    //         var myId = "id" + i + "id" + j;
+    //         var myIdr = "id" + i + "id" + (j + 1);
+    //         var myIdd = "id" + (i + 1) + "id" + j;
+    //         if(arr[i][j] == 0){
+    //             if(arr[i][j + 1] == 0){
+    //                 g.addEdge(myId, myIdr);
+    //             }
+    //             if(arr[i + 1][j] == 0){
+    //                 g.addEdge(myId, myIdd);
+    //             }
+    //         }
+    //     }
+    // }
